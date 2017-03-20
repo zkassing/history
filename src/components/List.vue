8af88bbd4b27c21d83e971d8f10ada8cb1046d24
kassing <kassing@live.cn>
2017-03-20 13:48:30 +0800

    组件布局

---
+++ src/components/List.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-if="items" v-for="item in items" :data-id="item.e_id">
+            <h3>{{item.title}}</h3>
+            <span>{{item.date}}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default{
+        props:["items"]
+    }
+</script>
+
+<style>
+    
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <List :items="items"></List>
+    </div>
+</template>
+
+<script>
+    import List from "./List.vue"
+    export default {
+        data () {
+            return {
+                items:[]
+            }
+        },
+        components:{
+            List
+        },
+        created(){
+            this.$http.jsonp("http://v.juhe.cn/todayOnhistory/queryEvent.php?key=7916c7a8e8e94ef7576ff1117dbc436d&dtype=jsonp&date=1/1").then(response=>{
+                this.items = response.body.result
+            })
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
